bdd1298546f772d59336e7519b947a9f97cbcd44
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2023-08-18 10:36:41 -0600

    yes

--- intro.py
+++ intro.py
@@ -12,16 +12,19 @@ print(price)
 
 #   name: john smith, age: 20 yrs old, patient: new
 print("Welcome to Hospital X")
-name = str(input("Please, enter your Name:"))
-Lastname = str(input("Please, enter your Lastname:"))
-age = int(input("Please, enter your Age:"))
-status = str(input("Are You a New Patient? (y)(n)"))
+Name = str(input("Please, enter your Name: "))
+Lastname = str(input("Please, enter your Lastname: "))
+Age = int(input("Please, enter your Age: "))
+status = str(input("Are You an (A)ctive or a (N)ew Patient? "))
 
 if status.upper() == "N":
-    print("Hello")
-    print("Welcome", name.capitalize(), Lastname.capitalize(), age, "New Patient")
-    print("Hospital X, Welcomes you")
+    Social = int(input("Please, Enter your Social: "))
+    print("Hello,")
+    print("Welcome,", Name.capitalize(), Lastname.capitalize())
+    print("New Patient,", "Age:", Age, "with Social:", Social)
+    print("Hospital X, Welcomes You")
 
-if status.upper() == "Y":
-    print("Hello")
-    print("Welcome", name.capitalize(), Lastname.capitalize(), age, "Hospital X, Welcomes you")
+if status.upper() == "A":
+    print("Hello,")
+    print("Welcome,", Name.capitalize(), Lastname.capitalize(), "Age:", Age)
+    print("Hospital X is Happy to See You!")
