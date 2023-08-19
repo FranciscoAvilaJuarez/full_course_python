6d17a59208283feee07a73f2552b8fcb311fd4ed
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2023-08-18 23:20:32 -0600

    yes

--- intro.py
+++ intro.py
@@ -1,3 +1,5 @@
+# Import date class from datetime module
+from datetime import date
 
 print("francisco avila")
 print("o_____o")
@@ -12,19 +14,31 @@ print(price)
 
 #   name: john smith, age: 20 yrs old, patient: new
 print("Welcome to Hospital X")
+
 Name = str(input("Please, enter your Name: "))
 Lastname = str(input("Please, enter your Lastname: "))
-Age = int(input("Please, enter your Age: "))
+Age = int(input("Enter the Year you were Born: "))
 status = str(input("Are You an (A)ctive or a (N)ew Patient? "))
+
+
+# Returns the current local date
+today = date.today()
+print(today)
+Edad = today.year - int(Age)    #nice, my idea
 
 if status.upper() == "N":
     Social = int(input("Please, Enter your Social: "))
     print("Hello,")
     print("Welcome,", Name.capitalize(), Lastname.capitalize())
-    print("New Patient,", "Age:", Age, "with Social:", Social)
+    print("New Patient,", "Age:", Edad, "with Social:", Social)
     print("Hospital X, Welcomes You")
 
 if status.upper() == "A":
     print("Hello,")
-    print("Welcome,", Name.capitalize(), Lastname.capitalize(), "Age:", Age)
+    print("Welcome,", Name.capitalize(), Lastname.capitalize(), "Age:", Edad)
     print("Hospital X is Happy to See You!")
+
+
+year_born: int = int(input("enter the year you were born "))
+age = today.year - year_born
+print(Name, "your age is:", age)
