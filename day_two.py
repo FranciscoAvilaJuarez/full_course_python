7dffa9f646ef0ac1473bf72e76fa060724166f37
FranciscoAvilaJuarez <francisco.salomon.avila.juarez@gmail.com>
2023-09-12 15:14:36 -0600

    [R-SM_F]

--- day_two.py
+++ day_two.py
@@ -2,13 +2,16 @@
 ## ask user their weight and convertl it and print it on terminal
 convert = float(0.450)
 
-print("hello, there! ")
-peso = int(input("what is your weight? "))
-tipo = str(input("is this in (K)g or (L)bs?"))
-if tipo == "k":
-    new_Pound = peso * convert
+print("Hello, There, Friend! ")
+Peso = int(input("What is your Weight? "))
+Tipo = str(input("Is this in (K)g or in (L)bs?"))
+
+#convert to pound
+if Tipo == "k":
+    new_Pound = Peso * convert
     print("this is your Weight in Pounds:", int(new_Pound))
 
-elif tipo == "l":
-    new_peso = peso/convert
+#convert to kilo
+elif Tipo == "l":
+    new_peso = Peso/convert
     print("This is your Weight in Kilos:", int(new_peso))
---
+++ re_startMosh/1_mosh.py
@@ -0,0 +1,12 @@
+
+# name = input( 'What is your Name? ')
+# print ('Hi, ' + name.title() + '!' )
+
+# EX 1  RESULT : A Paco le gusta el color Azul  NOTES: wanted to use f strings to make 'print' shorter/easier but no me las se yet
+Nombre = input('Hola, Cual es tu Nombre?_')
+Color_favorito = input( Nombre + ', Cual es tu color favorito?_' )
+print ( 'A_' + Nombre.title() + '_le gusta el color_' + Color_favorito.title() )
+
+#the year we were born  EX EXTRA, SELF
+Fecha_Nacimiento = int( input('What year were you born?_') )
+print( Nombre.title() + '_nacio en el año de_' + str(Fecha_Nacimiento))
